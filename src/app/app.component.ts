ecdce355ee4db07441b568ed81aaf14b90f623ca
varunrajtj <varunrajtj@gmail.com>
2021-12-28 13:07:34 -0400

    Incremental Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  value = 'World';
+  title = 'angular-material-tab-router';
+  navLinks: any[];
+  activeLinkIndex = -1;
+
+  constructor(private router: Router) {
+    this.navLinks = [
+      {
+        label: 'Home',
+        link: './home',
+        index: 0
+      }, {
+        label: 'About',
+        link: './about',
+        index: 1
+      }, {
+        label: 'Contact',
+        link: './contact',
+        index: 2
+      },
+      {
+        label: 'Gallery',
+        link: './gallery',
+        index: 3
+      }
+    ];
+  }
+  ngOnInit(): void {
+    this.router.events.subscribe((res) => {
+      this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from '@angular/forms';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from "./app.component";
-
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule, FormsModule, NgxGalleryModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonModule, MatIconModule, MatTabsModule],
+  declarations: [AppComponent, GalleryComponent, HomeComponent, ContactComponent, AboutComponent],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
